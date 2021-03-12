cc41acbdc92e34c32b3ef4f3f8efb7925fe31c6d
Lucifer Uchiha <rowla101@gmail.com>
2021-03-12 18:37:54 +0100

    Make workflow work again

--- src/test/java/ProjectSetupTest.java
+++ src/test/java/ProjectSetupTest.java
@@ -1,12 +1,11 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProjectSetupTest {
 
     @Test
     void testJunitConfiguration() {
-        assertFalse(true, "This test is to check if JUnit5 was correctly integrated into the project.");
+        assertTrue(true, "This test is to check if JUnit5 was correctly integrated into the project.");
     }
 }
