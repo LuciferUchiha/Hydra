c2120d8c381fcca47fff5a303d461a0dcc5b3c7c
Lucifer Uchiha <rowla101@gmail.com>
2021-03-12 18:12:45 +0100

    Added JUnit 5 and a basic Unit test

---
+++ src/test/java/ProjectSetupTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProjectSetupTest {
+
+    @Test
+    void testJunitConfiguration() {
+        assertTrue(true, "This test is to check if JUnit5 was correctly integrated into the project.");
+    }
+}
